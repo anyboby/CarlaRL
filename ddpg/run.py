542f2d1a2e5cccfec8614f111658952a726d065f
Florian Rottach <rottach@ids-hubbard>
2019-07-09 11:40:09 +0200

    removed other files

--- ddpg/run.py
+++
@@ -1,9 +0,0 @@
-import gym
-from ddpg import DDPG
-from gym.envs.box2d.car_racing import CarRacing
-
-env = CarRacing()
-ddpg = DDPG((15,15,1), 3, 10)
-ddpg.load_weights('_LR_0.0005_actor.h5', '_LR_0.0005_critic.h5')
-ddpg.train(env, render=True, batch_size=64, nb_episodes=10000)
-env.close()

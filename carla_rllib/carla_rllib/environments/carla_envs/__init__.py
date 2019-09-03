d94d28ff8dde6c4e9dd0bca91ee4cd48501e001a
Florian Rottach <rottach@mclaren>
2019-09-03 12:57:53 +0200

    cleanup
    Former-commit-id: cadd6b1e56c951e96ca39b85efd8146caec396f0

--- carla_rllib/carla_rllib/environments/carla_envs/__init__.py
+++
@@ -1,1 +0,0 @@
-from carla_rllib.environments.carla_envs.carla_env import BaseEnv
--- ddpg/CARLA/run.py
+++
@@ -1,9 +0,0 @@
-import gym
-import macad_gym
-from ddpg import DDPG
-env = gym.make("HomoNcomIndePOIntrxMASS3CTWN3-v0")
-
-ddpg = DDPG((15,15,1), 3, 10)
-#ddpg.load_weights('_LR_0.0005_actor.h5', '_LR_0.0005_critic.h5')
-ddpg.train(env, render=True, batch_size=64, nb_episodes=10000)
-env.close()

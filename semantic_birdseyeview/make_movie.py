518203ec8b49cab9e57514179c5c0181987d779f
anyboby <zanger.moritz@googlemail.com>
2019-10-19 12:21:44 +0200

    started writing in A3c
    Former-commit-id: 242bfea9a1bf56ce26fc232db7bf4bc54714a1ee

--- semantic_birdseyeview/make_movie.py
+++ semantic_birdseyeview/make_movie.py
@@ -1,4 +1,6 @@
 from utils import make_movie
+import numpy as np 
+from keras import backend as K
 
 DECIMATION = 2
 BATCH_SIZE = 10
@@ -32,10 +34,43 @@ sess = tf.Session(config=config) #----------------------------------------------
 set_session(sess) # set this TensorFlow session as the default session for Keras ----- ##
 ## ----------------------------------------------------------------------------------- ##
 
+def weighted_categorical_crossentropy(weights):
+    """
+    A weighted version of keras.objectives.categorical_crossentropy
+    
+    Variables:
+        weights: numpy array of shape (C,) where C is the number of classes
+    
+    Usage:
+        weights = np.array([0.5,2,10]) # Class one at 0.5, class 2 twice the normal weights, class 3 10x.
+        loss = weighted_categorical_crossentropy(weights)
+        model.compile(loss=loss,optimizer='adam')
+    """
+    
+    #you need this to load the model (weird, keras!)
+    weights = K.variable(weights)
+        
+    def loss(y_true, y_pred):
+        # scale predictions so that the class probas of each sample sum to 1
+        y_pred /= K.sum(y_pred, axis=-1, keepdims=True)
+        # clip to prevent NaN's and Inf's
+        y_pred = K.clip(y_pred, K.epsilon(), 1 - K.epsilon())
+        # calc
+        loss = y_true * K.log(y_pred) * weights
+        loss = -K.sum(loss, -1)
+        return loss
+    
+    return loss
+
+
+
 from keras.models import load_model
 
-model_filename = "models/multi_model__sweep=7_decimation=2_numclasses=3_valloss=0.225.h5"
-multi_model = load_model(model_filename)
+model_filename = "models/multi_model_rgb_sweep=18_decimation=2_numclasses=3_valloss=0.059.h5"
+weights_loss = np.array([0.18,0.18,0.64])
+weighted_ce_loss =  weighted_categorical_crossentropy(weights_loss)
+
+multi_model = load_model(model_filename, custom_objects={"loss":weighted_ce_loss})
 multi_model.summary()
 racetrack = "Town05"
 episodes = [13, 34, 45, 87, 90, 109] 
@@ -46,6 +81,7 @@ episodes = [13, 34, 45, 87, 90, 109]
 # for semantic segmentation
 # for episode in episodes:
 #     make_movie(model_filename, racetrack, episode, DECIMATION, CLASSES_NAMES, CAMERA_IDS, episode_len=1000, batch_size=BATCH_SIZE)
-#for rgb
+#for rgb (cmap none)
 for episode in episodes:
-    make_movie(model_filename, racetrack, episode, DECIMATION, CLASSES_NAMES, CAMERA_IDS_RGB, episode_len=1000, batch_size=BATCH_SIZE, cmap=None)
+    make_movie(model_filename, racetrack, episode, DECIMATION, CLASSES_NAMES, CAMERA_IDS_RGB, multi_model=multi_model, episode_len=1000, batch_size=BATCH_SIZE, cmap=None)
+
